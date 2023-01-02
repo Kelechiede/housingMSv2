7200642afbf7aef2b7fd5717d328c34334f6b85b
Ede kelechukwu Innocent <kelechukwuede@gmail.com>
2023-01-02 06:21:46 +0100

    First Commit

---
+++ src/main/java/com/kelechukwu/housingMSv2/HousingMSv2Application.java
@@ -0,0 +1,14 @@
+package com.kelechukwu.housingMSv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingMSv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingMSv2Application.class, args);
+		System.out.println("Good MAN");
+	}
+
+}
---
+++ src/test/java/com/kelechukwu/housingMSv2/HousingMSv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelechukwu.housingMSv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingMSv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
